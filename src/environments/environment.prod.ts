ee4956ffdc39a70d77041931fbb7164475dc3c48
paulsukow <paul.sukow@gmail.com>
2021-02-04 06:50:25 -0700

    setup firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBk9MV7PUdPDR_7AtxzLtE-wMhgV5NWZjo',
+    authDomain: 'to-do-list-9850c.firebaseapp.com',
+    projectId: 'to-do-list-9850c',
+    storageBucket: 'to-do-list-9850c.appspot.com',
+    messagingSenderId: '648078807844',
+    appId: '1:648078807844:web:108a60a0efb5264e8119b1',
+    measurementId: 'G-D01C3LT892'
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBk9MV7PUdPDR_7AtxzLtE-wMhgV5NWZjo',
+    authDomain: 'to-do-list-9850c.firebaseapp.com',
+    projectId: 'to-do-list-9850c',
+    storageBucket: 'to-do-list-9850c.appspot.com',
+    messagingSenderId: '648078807844',
+    appId: '1:648078807844:web:108a60a0efb5264e8119b1',
+    measurementId: 'G-D01C3LT892'
+  },
 };
 
 /*
