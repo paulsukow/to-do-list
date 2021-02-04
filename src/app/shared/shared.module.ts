ef8f254fbf8e61b7e9e8e2e79328089778859f28
paulsukow <paul.sukow@gmail.com>
2021-02-04 07:19:42 -0700

    create basic app structure

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProfileComponent,
+    LoginComponent,
+  ],
+  imports: [
+    CommonModule,
+    IonicModule
+  ],
+  exports: [
+    ProfileComponent,
+    LoginComponent,
+  ],
+})
+export class SharedModule { }
